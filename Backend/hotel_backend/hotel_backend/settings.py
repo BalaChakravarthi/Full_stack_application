607fc42db797803b99acd9ec7557f2e2d55b34df
BalaChakravarthi <balachakravarthidhupati@gmail.com>
2026-03-06 19:18:42 +0530

    Enable real email sending

--- Backend/hotel_backend/hotel_backend/settings.py
+++ Backend/hotel_backend/hotel_backend/settings.py
@@ -158,11 +158,11 @@ import os
 
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 SECRET_KEY = os.getenv("SECRET_KEY")
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# # print("EMAIL_HOST_USER:", EMAIL_HOST_USER)
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# print("EMAIL_HOST_USER:", EMAIL_HOST_USER)
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
